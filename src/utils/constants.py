d98828ff72cca36c36df7451dc729826a31bb1d1
sk1lizz <salimgareewruslan@gmail.com>
2026-04-25 16:20:50 +0300

    feat: добавлены константы для логов

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -15,6 +15,18 @@ APP_AUTHOR = [
 APP_YEAR = "2026"
 APP_URL = "https://github.com/Sk1lizz/KeyKeeper"
 
+
 # ===================================================
-#
+# Настройки логирования
 # ===================================================
+
+LOG_CONSOLE_LEVEL = 20
+LOG_FILE_LEVEL = 10
+
+LOG_FORMAT = "[%(levelname)s] %(asctime)s - %(message)s"
+LOG_DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+LOG_FILE = "keykeeper.log"
+
+LOG_MAX_BYTES = 5 * 1024 * 1024
+LOG_BACKUP_COUNT = 10               # На релизе изменить на 3/5
