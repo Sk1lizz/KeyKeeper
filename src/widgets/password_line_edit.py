4c4b524c6848a7984718d383c0dbc48510be6262
Sk1lizz <salimgareewruslan@gmail.com>
2026-05-17 21:15:34 +0300

    feat: курсор при наведении на кнопку видимости пароля

--- src/widgets/password_line_edit.py
+++ src/widgets/password_line_edit.py
@@ -5,7 +5,7 @@
 
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QLineEdit, QPushButton
 from PySide6.QtCore import Signal, Qt
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, QCursor
 
 
 class PasswordLineEdit(QWidget):
@@ -34,30 +34,31 @@ class PasswordLineEdit(QWidget):
         self.toggle_btn.setFixedHeight(25)
         self.toggle_btn.setCheckable(True)
         self.toggle_btn.clicked.connect(self._toggle_visibility)
+        self.toggle_btn.setCursor(QCursor(Qt.CursorShape.PointingHandCursor))
         layout.addWidget(self.toggle_btn)
 
-    
+
     def _toggle_visibility(self, checked):
         """"""
-        
+
         if checked:
             self.line_edit.setEchoMode(QLineEdit.Normal)
             self.toggle_btn.setText("🔒")
-        
+
         else:
             self.line_edit.setEchoMode(QLineEdit.Password)
             self.toggle_btn.setText("👁")
 
-    
+
     def text(self) -> str:
         return self.line_edit.text()
-    
+
     def setText(self, text: str):
         self.line_edit.setText(text)
 
     def clear(self):
         self.line_edit.clear()
-    
+
     def setPlaceholderText(self, text: str):
         self.line_edit.setPlaceholderText(text)
 
@@ -71,7 +72,7 @@ class PasswordLineEdit(QWidget):
 
     def isReadOnly(self) -> bool:
         return self.line_edit.isReadOnly()
-    
+
     def setFocus(self):
         self.line_edit.setFocus()
 
